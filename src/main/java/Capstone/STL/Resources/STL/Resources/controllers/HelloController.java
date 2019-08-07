fbba95fd7a869d59ae23892787946ac153228b0d
Janelle Burger <janelle.burger333@gmail.com>
2019-08-06 19:48:13 -0500

    Basic "Hello, World" application

--- src/main/java/Capstone/STL/Resources/STL/Resources/controllers/HelloController.java
+++ src/main/java/Capstone/STL/Resources/STL/Resources/controllers/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-
+//Hello World Application 
     @RequestMapping(value = "")
     @ResponseBody
     public String index() {
